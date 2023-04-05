8387f405c5f17f6c218ebc8f82a81f2cf2f76b66
clintjeez <clintjeez@gmail.com>
2023-04-05 16:41:36 +0100

    initial setup

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="font-bold underline">Merchants UI</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/views/Dashboard/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MerchantsDashboard = () => {
+  return <div>MerchantsDashboard</div>;
+};
+
+export default MerchantsDashboard;
