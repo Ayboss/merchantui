821777a70bb0252db087bd4553bfbbfc94f76a99
clintjeez <clintjeez@gmail.com>
2023-04-15 14:34:38 +0100

    dashboard routes updates initial

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,15 +2,27 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import Dashboard from "../views/Dashboard";
 import Login from "../views/Auth/Login";
 import Signup from "../views/Auth/Signup";
+import GetStarted from "../views/Dashboard/GetStarted";
 
 export const AppRouter = () => {
   return (
     <Routes>
-      <Route path="/" element={<Dashboard />} />
+      <Route path="dashboard" element={<Dashboard />}>
+        <Route path="getstarted" element={<GetStarted />} />
+      </Route>
+
       <Route path="login" element={<Login />} />
       <Route path="signup" element={<Signup />} />
-
       <Route path="*" element={<Navigate to="/login" replace />} />
+      <Route path="getstarted" element={<GetStarted />} />
+    </Routes>
+  );
+};
+
+export const DashboardRoutes = () => {
+  return (
+    <Routes>
+      {/* <Route path="getstarted" element={<GetStarted />} /> */}
     </Routes>
   );
 };
--- src/views/Dashboard/index.tsx
+++ src/views/Dashboard/index.tsx
@@ -14,6 +14,7 @@ import DashboardLayout from "../../layouts/DashboardLayout";
 import { NavLink } from "react-router-dom";
 import { NavLinks } from "../../constants";
 import DashboardTopbar from "../../components/DashboardTopbar";
+import { DashboardRoutes } from "../../routes";
 
 const MerchantsDashboard = () => {
   return (
@@ -35,6 +36,7 @@ const MerchantsDashboard = () => {
       </DashboardSidebar>
       <DashboardBody>
         <DashboardTopbar />
+        <DashboardRoutes />
       </DashboardBody>
     </DashboardLayout>
   );
