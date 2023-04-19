fa6fdaa6a3a35d3567bf2c0efb8c5ea9bb66ff61
clintjeez <clintjeez@gmail.com>
2023-04-19 16:03:58 +0100

    logo information form container setup

---
+++ src/views/Dashboard/RegisteredBusinessFlow/Flows/BusinessInformation/Steps/Logo/index.tsx
@@ -0,0 +1,13 @@
+import { Title } from "../BasicInformation/styles";
+import { LogoInformationContainer, LogoInformationForm } from "./styles";
+
+const LogoInformation = () => {
+  return (
+    <LogoInformationContainer>
+      <Title>BUSINESS LOGO</Title>
+      <LogoInformationForm></LogoInformationForm>
+    </LogoInformationContainer>
+  );
+};
+
+export default LogoInformation;
---
+++ src/views/Dashboard/RegisteredBusinessFlow/Flows/BusinessInformation/Steps/Logo/styles.ts
@@ -0,0 +1,4 @@
+import tw from "tailwind-styled-components";
+
+export const LogoInformationContainer = tw.div``;
+export const LogoInformationForm = tw.form``;
