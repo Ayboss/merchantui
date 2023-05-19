997e51b0e3f2d0e2a484f7d115b5a41782e5af6c
clintjeez <clintjeez@gmail.com>
2023-05-19 09:29:34 +0100

    forget password ui updates

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -51,7 +51,7 @@ export const AppRouter = () => {
       <Route path='*' element={<Navigate to='/login' replace />} />
 =======
       <Route path="login" element={<Login />} />
-      <Route path="forgetpassword" element={<ForgetPassword />} />
+      <Route path="forget-password" element={<ForgetPassword />} />
       <Route path="signup" element={<Signup />} />
       <Route path="*" element={<Navigate to="/login" replace />} />
 >>>>>>> 5869871 (forget password updates with new branch initialization)
