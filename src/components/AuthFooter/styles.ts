132fb6ab05790392aa8de0b2bb88ef2014cf5696
clintjeez <clintjeez@gmail.com>
2023-06-03 10:19:29 +0100

    refactor: auth footer mobile responsive screen and partners icons removed

--- src/components/AuthFooter/styles.ts
+++ src/components/AuthFooter/styles.ts
@@ -1,31 +1,16 @@
 import tw from 'tailwind-styled-components';
-import styled from 'styled-components';
-import bgImg from '../../assets/img/footer_bg.svg';
 
-export const AuthFooterContainer = styled.footer`
-  height: 313px;
-  background-image: url(${bgImg});
-  background-color: #6231f4;
-  background-repeat: no-repeat;
-  background-position: right;
-  background-size: contain;
-  padding: 72px 100px 0px 100px;
-  color: #fff;
-  flex-shrink: 0;
-`;
-
-export const AuthFooterText = tw.p` 
-text-[16px]
-leading-[15px]
-font-sans
-font-semibold
-`;
-export const AuthFooterImages = tw.div`
-flex
-gap-10
-justify-start
-mt-[24px]
-`;
-export const AuthFooterImg = tw.img`
-w-[100px]
+export const AuthFooterContainer = tw.footer`
+  hidden
+  lg:block
+  h-[313px] 
+  bg-[url('@/assets/img/footer_bg.svg')] 
+  bg-[#6231f4] 
+  bg-no-repeat
+  bg-right
+  bg-contain
+  pt-[72px]
+  px-[100px]
+  text-[#fff]
+  shrink-0
 `;
