5b3686f25774f71c583252ee1b363eb25e65270b
clintjeez <clintjeez@gmail.com>
2023-06-12 23:32:03 +0100

    feat: business logo file upload service hook up

--- src/views/Dashboard/Onboarding/RegisteredBusinessFlow/Flows/BusinessInformation/Steps/Logo/index.tsx
+++ src/views/Dashboard/Onboarding/RegisteredBusinessFlow/Flows/BusinessInformation/Steps/Logo/index.tsx
@@ -1,13 +1,32 @@
 import React, { useState } from 'react';
 import { Title } from '../BasicInformation/styles';
 import { Button, UploadInput } from '../../../../../../../../components';
+import {
+  UploadRequestPayloadType,
+  useUploadMutationsQuery
+} from '../../../../../../../../services/hooks/useUploadMutationsQuery';
 import { LogoInformationContainer, LogoInformationForm } from './styles';
 
 const LogoInformation = ({ onNext }: any) => {
+  const { isLoading, mutateAsync } = useUploadMutationsQuery();
   const [uploadedFile, setUploadedFile] = useState<File | null>(null);
 
   // eslint-disable-next-line no-console
   console.log(uploadedFile);
+  const handleUpload = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    mutateAsync(uploadedFile as UploadRequestPayloadType)
+      .then((data) => {
+        // eslint-disable-next-line no-console
+        console.log(data);
+      })
+      .catch((error) => {
+        // eslint-disable-next-line no-console
+        console.log(error);
+      });
+
+    onNext();
+  };
 
   const handleFileSelect = (file: File | null) => {
     setUploadedFile(file);
@@ -16,12 +35,13 @@ const LogoInformation = ({ onNext }: any) => {
   return (
     <LogoInformationContainer>
       <Title>BUSINESS LOGO</Title>
-      <LogoInformationForm>
+      <LogoInformationForm onSubmit={handleUpload}>
         <UploadInput name='businessLogo' onFileSelect={handleFileSelect} />
         <Button
           name='Save & Continue'
           className='bg-[#D3D3D3] text-[#2A2A2A] text-[16px] font-bold'
-          onClick={onNext}
+          isBusy={isLoading}
+          type='submit'
         />
       </LogoInformationForm>
     </LogoInformationContainer>
