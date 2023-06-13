c6429d3f9376a30854410bc32320ad08c0c1a1cd
clintjeez <clintjeez@gmail.com>
2023-06-13 01:57:17 +0100

    feat: merchant social data endpoint hooked up

--- src/components/URLInput/index.tsx
+++ src/components/URLInput/index.tsx
@@ -1,17 +1,20 @@
-import React from 'react';
+import React, { forwardRef } from 'react';
 import { FormGroup, InputLabel, Input, Container, SocialIcons } from './styles';
 import { URLInputProps } from './type';
 
-export const URLInput: React.FC<URLInputProps> = ({ label, name, placeholder, icon }) => {
+export const URLInput: React.FC<URLInputProps> = forwardRef((props, ref) => {
+  const { label, name, placeholder, icon, ...rest } = props;
+
   return (
     <FormGroup>
       <InputLabel>{label}</InputLabel>
       <Container>
         <SocialIcons src={icon} alt='social icon' />
-        <Input name={name} placeholder={placeholder} />
+        {/* @ts-ignore no overload matches for the ref */}
+        <Input name={name} type='url' placeholder={placeholder} ref={ref} {...rest} />
       </Container>
     </FormGroup>
   );
-};
+});
 
 export default URLInput;
