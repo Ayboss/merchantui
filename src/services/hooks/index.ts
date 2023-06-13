58d78d5b4e875437deb0f52ec15977ec381a0d0a
clintjeez <clintjeez@gmail.com>
2023-06-13 01:03:03 +0100

    feat: settings endpoint query setup

--- src/services/hooks/index.ts
+++ src/services/hooks/index.ts
@@ -5,10 +5,7 @@ export * from './useGetPayoutsQuery';
 export * from './useVerifyBankAccount';
 export * from './useGetBanksQuery';
 export * from './useSendMoney';
-<<<<<<< HEAD
 export * from './useSettingsMutation';
 export * from './useBvnMutation';
 export * from './useCreateSettlementMutation';
-=======
 export * from './useCreateWallet';
->>>>>>> 81ad2d6 (feat: created wallet queries)
