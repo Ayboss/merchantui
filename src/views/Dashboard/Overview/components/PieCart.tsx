af0f969395a5d0ec6576851c6a597d49c38ffa8c
Olatunde Owokoniran <hurlatunde@gmail.com>
2023-06-16 09:26:37 +0100

    fix(dashboard): pie fixed colours and add default data

--- src/views/Dashboard/Overview/components/PieCart.tsx
+++ src/views/Dashboard/Overview/components/PieCart.tsx
@@ -18,7 +18,7 @@ import { PieCartType } from './types';
 
 export const PieCart: React.FC<PieCartType> = (props) => {
   const { startDate, endDate } = props;
-  const { data, isLoading, refetch } = useOverviewPieChartQuery({
+  const { data, isLoading, refetch, isRefetching } = useOverviewPieChartQuery({
     startDate,
     endDate
   });
@@ -37,7 +37,7 @@ export const PieCart: React.FC<PieCartType> = (props) => {
 
   return (
     <Wrapper>
-      {isLoading ? (
+      {isLoading || isRefetching ? (
         <div className={`h-[319px] relative`}>
           <div
             className={`w-full h-full flex flex-col justify-center items-center absolute overlay`}
@@ -73,7 +73,10 @@ export const PieCart: React.FC<PieCartType> = (props) => {
             <Wrapper className='grid grid-cols-1'>
               {pieChartData.map((pieChart, id) => (
                 <FlexWrapper key={id}>
-                  <Box className={`bg-[${colorBars[pieChart.x as colorKey]}]`} />
+                  <Box
+                    className={`w-[20px] h-[20px] mr-3 border-radius-[2px]`}
+                    style={{ backgroundColor: colorBars[pieChart.x as colorKey] }}
+                  />
                   <Wrapper>
                     <BoxPercentageTitle>{pieChart.x}</BoxPercentageTitle>
                     <BoxPercentage>{pieChart.y}%</BoxPercentage>
